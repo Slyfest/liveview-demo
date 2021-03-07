48ecde60ddf9400b49689a53b5515ccca1f9558b
Slyfest <mellowstripe@gmail.com>
2021-03-07 16:06:59 +0600

    Add event handlers for reposts and likes

--- lib/live_demo_web/live/post_live/post_component.ex
+++ lib/live_demo_web/live/post_live/post_component.ex
@@ -17,10 +17,14 @@ defmodule LiveDemoWeb.PostLive.PostComponent do
 
       <div class="row">
         <div class="column">
-          <i class="far fa-heart"></i> <%= @post.likes_count %>
+          <a href="#" phx-click="like" phx-target="<%= @myself %>">
+            <i class="far fa-heart"></i> <%= @post.likes_count %>
+          </a>
         </div>
         <div class="column">
-          <i class="far fa-hand-peace"></i> <%= @post.reposts_count %>
+          <a href="#" phx-click="repost" phx-target="<%= @myself %>">
+            <i class="far fa-hand-peace"></i> <%= @post.reposts_count %>
+          </a>
         </div>
         <div class="column post-button-column">
           <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do %>
@@ -35,5 +39,15 @@ defmodule LiveDemoWeb.PostLive.PostComponent do
 
     </div>
     """
+  end
+
+  def handle_event("like", _, socket) do
+    LiveDemo.Timeline.inc_likes(socket.assigns.post)
+    {:noreply, socket}
+  end
+
+  def handle_event("repost", _, socket) do
+    LiveDemo.Timeline.inc_reposts(socket.assigns.post)
+    {:noreply, socket}
   end
 end
